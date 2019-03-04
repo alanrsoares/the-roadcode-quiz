bbe52ce34e8c5ca10745caa9eb90c287f10f9fd1
Alan Soares <alanrsoars@gmail.com>
2019-03-05 10:07:32 +1300

    better generics

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,11 +1,11 @@
 const LS_KEY = "@THE_ROAD_QUIZ";
 
 export class Storage {
-  static persist(value: Object) {
+  static persist<T extends {}>(value: T) {
     localStorage.setItem(LS_KEY, JSON.stringify(value));
   }
 
-  static read<T>(defaultValue: T): T {
+  static read<T extends {}>(defaultValue: T): T {
     const value = localStorage.getItem(LS_KEY);
     return value !== null ? JSON.parse(value) : defaultValue;
   }
