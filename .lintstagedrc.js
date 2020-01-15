7ad171ce0ac05a6e07b20c59553609c86f601f24
Alan Soares <alanrsoars@gmail.com>
2020-01-15 16:31:39 +1300

    try break it again

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --write", "git add"],
-  "src/**/*.{ts,tsx}(x)": () => "tsc -p tsconfig.json --noEmit"
+  "src/**/*.{ts,tsx}(x)": () => "tsc -p tsconfig.json --noEmit",
+  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --write", "git add"]
 };
