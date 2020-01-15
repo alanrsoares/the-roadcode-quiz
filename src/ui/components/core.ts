b0319f88522157283c98f31cdcfc2ab6b9a7076e
Alan Soares <alanrsoars@gmail.com>
2020-01-15 16:24:14 +1300

    try break it again

--- src/ui/components/core.ts
+++ src/ui/components/core.ts
@@ -118,7 +118,7 @@ export const Image = styled.img<{ round?: boolean }>`
   height: 8em;
   overflow: hidden;
   border-radius: ${getRadius(props => (props.round ? "round" : "md"))};
-  border: solid 0.2em ${getColor("trybreakitevenmore")};
+  border: solid 0.2em ${getColor("wtflol")};
   transition: border-radius 0.2s linear;
 `;
 
