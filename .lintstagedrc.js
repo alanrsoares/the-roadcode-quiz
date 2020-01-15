865173e35676776777aa8a2545432eda7c1edd75
Alan Soares <alanrsoars@gmail.com>
2020-01-15 17:01:57 +1300

    simplify

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "**/*.ts?(x)": () => ["tsc -p tsconfig.json --noEmit"],
+  "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
   "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --write", "git add"]
 };
