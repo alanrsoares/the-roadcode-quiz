44ef0b6033c7ce2b25635bd5cb8ae786056e213c
Alan Soares <alanrsoars@gmail.com>
2022-11-09 10:26:18 +1300

    chore: upgrade all the things

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,7 +2,7 @@ import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
 import Storage from "lib/StorageAdapter";
 
-export function useCachedState<T>(
+export function useCachedState<T extends {}>(
   cacheKey: string,
   defaultState: T
 ): [T, Dispatch<SetStateAction<T>>] {
