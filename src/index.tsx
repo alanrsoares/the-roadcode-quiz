aa474c98fee9f8fbf8c01579d56b05266614f7f0
alanrsoares <alan@axelar.network>
2024-08-16 08:42:37 +1200

    chore: upgrade a few other packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 
-import reportWebVitals from "reportWebVitals";
 import registerServiceWorker from "registerServiceWorker";
 
 import Splash from "ui/compounds/Splash";
@@ -26,15 +25,5 @@ declare global {
     gtag(method: string, event: string, meta: Record<string, any>): void;
   }
 }
-
-reportWebVitals(({ id, name, value }) => {
-  window.gtag("send", "event", {
-    eventCategory: "Web Vitals",
-    eventAction: name,
-    eventValue: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    eventLabel: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate
-  });
-});
 
 registerServiceWorker();
